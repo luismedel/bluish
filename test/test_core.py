8595c6b9898b58930a1e56bfeaf82558fd9cb3f6
Luis Medel <luis@luismedel.com>
2024-09-02 16:48:35 +0200

    Use base64 for file transfers

--- test/test_core.py
+++ test/test_core.py
@@ -351,12 +351,30 @@ jobs:
             - uses: core/expand-template
               with:
                   hide_output: true
-                  input: "Hello, ${{{{ WORLD }}}}"
+                  input: |
+                      En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho
+                      tiempo que vivía un hidalgo de los de lanza en astillero, adarga antigua,
+                      rocín flaco y galgo corredor. Una olla de algo más vaca que carnero,
+                      salpicón las más noches, duelos y quebrantos los sábados, lantejas los
+                      viernes, algún palomino de añadidura los domingos, consumían las tres
+                      partes de su hacienda. El resto della concluían sayo de velarte, calzas de
+                      velludo para las fiestas, con sus pantuflos de lo mesmo, y los días de
+                      entresemana se honraba con su vellorí de lo más fino. Tenía en su casa una
+                      ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte,
+                      y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la
+                      podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años; era de
+                      complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo
+                      de la caza. Quieren decir que tenía el sobrenombre de Quijada, o Quesada,
+                      que en esto hay alguna diferencia en los autores que deste caso escriben;
+                      aunque, por conjeturas verosímiles, se deja entender que se llamaba
+                      Quejana. Pero esto importa poco a nuestro cuento; basta que en la narración
+                      dél no se salga un punto de la verdad.
+                      Hello, ${{{{ WORLD }}}}
                   output_file: {temp_file.name}
     """)
         pipe.dispatch()
-        assert pipe.jobs["expand_template"].output == "Hello, World!"
-        assert run(f"cat {temp_file.name}").stdout.strip() == "Hello, World!"
+        assert pipe.jobs["expand_template"].output.endswith("Hello, World!")
+        assert run(f"tail -n1 {temp_file.name}").stdout.strip() == "Hello, World!"
 
 
 def test_pass_env() -> None:
