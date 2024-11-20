8b11116633284adcac47e6c072e96d331e2110c5
Luis Medel <luis@luismedel.com>
2024-11-21 00:53:22 +0100

    Add tests for new schema validation

--- test/test_schemas.py
+++ test/test_schemas.py
@@ -1,115 +1,93 @@
 
-from typing import Any
-
-from bluish.schemas import KV, InvalidTypeError, RequiredAttributeError, validate_schema
+from bluish.schemas import DefaultDict, Dict, AnyType, List, Object, Optional, Str, InvalidTypeError, RequiredAttributeError
 
 
 def test_happy() -> None:
-    schema = {
-        "type": dict,
-        "properties": {
-            "name": [str, None],
-            "env": [KV, None],
-        },
-    }
+    schema = Object({
+        "name": Str,
+        "env": DefaultDict,
+    })
 
-    validate_schema(schema, {
+    schema.validate({
         "name": "test",
         "env": {"a": "b"},
     })
-    
-    validate_schema(schema, {
-        "name": "test",
+
+
+def test_default_values() -> None:
+    schema = Object({
+        "name": Str,
+        "env": DefaultDict,
+        "items": List(Str, default=["a", "b"]),
     })
-
-
-def test_missing_key() -> None:
-    schema = {
-        "type": dict,
-        "properties": {
-            "name": str,
-            "env": KV,
-        },
-    }
 
     data = {
         "name": "test",
     }
-    
+    # This should add the default values for "env" and "items"
+    schema.validate(data)
+
+    assert "env" in data
+    assert data["env"] == {}
+
+    assert "items" in data
+    assert data["items"] == ["a", "b"]
+
+
+def test_missing_key() -> None:
+    schema = Object({
+        "name": Str,
+        "env": Dict,
+    })
+
     try:
-        validate_schema(schema, data)
+        schema.validate({
+            "name": "test",
+        })
         raise AssertionError("Should have raised an exception")
     except RequiredAttributeError:
         pass
 
 
 def test_optional_key() -> None:
-    schema = {
-        "type": dict,
-        "properties": {
-            "name": [str, None],
-        },
-    }
+    schema = Object({
+        "name": Optional(Str),
+    })
 
-    data: dict = {
-    }
-
-    validate_schema(schema, data)
-
+    schema.validate({})
+    schema.validate({
+        "name": "test",
+    })
 
 def test_lists() -> None:
-    schema = {
-        "type": dict,
-        "properties": {
-            "values": {
-                "type": list,
-                "item_schema": str,
-            },
-        },
-    }
+    schema = Object({
+        "values": List(Str),
+    })
     
-    data = {
+    schema.validate({
         "values": ["a", "b"],
-    }
-    
-    validate_schema(schema, data)
+    })
 
 
 def test_lists_any() -> None:
-    schema = {
-        "type": dict,
-        "properties": {
-            "values": {
-                "type": list,
-                "item_schema": Any,
-            },
-        },
-    }
+    schema = Object({
+        "values": List(AnyType),
+    })
 
-    data = {
+    schema.validate({
         "values": ["a", "b", 1, {"a": "b"}, [1, 2, 3]],
-    }
-    
-    validate_schema(schema, data)
+    })
 
 
 def test_lists_ko() -> None:
-    schema = {
-        "type": dict,
-        "properties": {
-            "values": {
-                "type": list,
-                "item_schema": str,
-            },
-        },
-    }
+    schema = Object({
+        "values": List(Str),
+    })
 
-    data = {
-        "values": ["a", "b", 1],
-    }
-    
     try:
-        validate_schema(schema, data)
+        schema.validate({
+            "values": ["a", "b", 1],
+        })
         raise AssertionError("Should have raised an exception")
     except InvalidTypeError:
         pass
--- test/utils.py
+++ test/utils.py
@@ -5,5 +5,5 @@ import yaml
 
 
 def create_workflow(yaml_definition: str) -> bluish.contexts.workflow.WorkflowContext:
-    definition = WorkflowDefinition(yaml.safe_load(yaml_definition))
+    definition = WorkflowDefinition(**yaml.safe_load(yaml_definition))
     return bluish.contexts.workflow.WorkflowContext(definition)
